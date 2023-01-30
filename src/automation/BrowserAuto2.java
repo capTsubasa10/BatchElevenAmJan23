d1ef5a371ce658c1111b59d8715ccd2d747c9fb9
A2ZpracticeToday <A2ZpracticeToday@gmail.com>
2023-01-30 11:40:06 +0530

    First Project - 30/01/2023

---
+++ src/automation/BrowserAuto2.java
@@ -0,0 +1,16 @@
+package automation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserAuto2 {
+
+	public static void main(String[] args) {
+		
+		//Chrome
+		ChromeDriver driver = new ChromeDriver();
+		
+		
+
+	}
+
+}
---
+++ src/automation/BrowserAutomation.java
@@ -0,0 +1,18 @@
+package automation;
+
+
+
+public class BrowserAutomation {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		
+
+		
+		
+
+	}
+
+}
